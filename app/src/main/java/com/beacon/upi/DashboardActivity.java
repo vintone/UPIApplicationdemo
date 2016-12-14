b2d80ebf93d0e52d8701fc2827d6b2bb38d8bb6a
vinod <vinod@tonetag.com>
2016-12-14 13:23:39 +0530

    Initial commit

---
+++ app/src/main/java/com/beacon/upi/DashboardActivity.java
@@ -0,0 +1,20 @@
+package com.beacon.upi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by vinod on 14/12/16.
+ */
+
+public class DashboardActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/beacon/upi/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.beacon.upi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by vinod on 14/12/16.
+ */
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
